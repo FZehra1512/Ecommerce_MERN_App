34ea17256a423424ecb53cd5a51906b39827a376
Fatima Zehra <fz8585949@gmail.com>
2024-08-15 12:10:25 +0500

    Created basic frontend setup

---
+++ frontend/src/pages/home/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <div>This is home page</div>
+        <button>This is a button</button>
+    </div>
+  )
+}
+
+export default Home
