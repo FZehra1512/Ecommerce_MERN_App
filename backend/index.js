91311307156ec01258176959e2ccc3871a115aa1
Fatima Zehra <fz8585949@gmail.com>
2024-08-16 16:49:36 +0500

    Get product api

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
+import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
 const app = express();
@@ -18,6 +19,9 @@ connectDB();
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
+
+// Product routes
+app.use("/api", productRoutes);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
